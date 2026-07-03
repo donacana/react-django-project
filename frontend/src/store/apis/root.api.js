d52e59e9f83ebde9ecce88b3c40d1a202b78b2f4
donacana <sooam.park@gmail.com>
2026-07-03 18:19:38 +0900

    fix frontend api base url

--- frontend/src/store/apis/root.api.js
+++ frontend/src/store/apis/root.api.js
@@ -1,18 +1,25 @@
 import axios from "axios";
 
 export const rootApi = axios.create({
-    baseURL: "http://localhost:8000/api",
+    // 배포 환경에서는 Nginx가 /api 요청을 Django로 넘김
+    baseURL: "/api",
 });
 
-rootApi.interceptors.request.use((config) => {
+rootApi.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("accessToken");
 
-    const token = localStorage.getItem("accessToken");
+        console.log("TOKEN =", token);
 
-    console.log("TOKEN =", token);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
 
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
+);
 
-    return config;
-});
+export default rootApi;
